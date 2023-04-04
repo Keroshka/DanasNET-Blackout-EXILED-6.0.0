cb2d03a35696da14594dc9a70b6053bff88c8892
Ivan Kerovec <ivankerovec2@gmail.com>
2023-04-04 20:00:31 +0200

    Add project files.

---
+++ DanasNET-Blackout/EventHandlers.cs
@@ -0,0 +1,14 @@
+﻿namespace DanasNET_Blackout
+{
+    public class EventHandlers
+    {
+        private Plugin _plugin;
+
+        public EventHandlers(Plugin plugin)
+        {
+            _plugin = plugin;
+        }
+
+
+    }
+}
---
+++ DanasNET-Blackout/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanasNET_Blackout
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "Keroshka";
+        public override string Name { get; } = "DanasDotNET-Blackout";
+        public override Version Version { get; } = new Version(1, 0, 0);
+
+        public override Version RequiredExiledVersion { get; } = new Version(6, 0, 0);
+
+        public static Plugin Instance = new Plugin();
+
+        public EventHandlers EventHandler { get; private set; }
+
+        public Random rng = new Random();
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+    }
+}
